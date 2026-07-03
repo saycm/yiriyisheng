8c00dfe73dee7e7f88a0aaf37163264ce786314b
cm <sayskycm@gmail.com>
2026-07-03 23:43:06 +0800

    chore: bump app version to 1.0.32

--- lib/core/app_core.dart
+++ lib/core/app_core.dart
@@ -35,11 +35,11 @@ const String apiBaseUrl = String.fromEnvironment(
 );
 const String appVersionName = String.fromEnvironment(
   'PINGSHENG_APP_VERSION_NAME',
-  defaultValue: '1.0.31',
+  defaultValue: '1.0.32',
 );
 const int appVersionCode = int.fromEnvironment(
   'PINGSHENG_APP_VERSION_CODE',
-  defaultValue: 32,
+  defaultValue: 33,
 );
 
 final RegExp authHiddenOrWhitespacePattern =
